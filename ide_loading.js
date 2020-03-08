b501eeeb770ba624bc03cb1c457507963bc1143b
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-03-08 09:39:47 +0900

    ar

--- ide_loading.js
+++ ide_loading.js
@@ -1,8 +1,6 @@
-var N1=1000;
-var N2=40;
-$(function(){
-	$('.ide_loading').on('click',function(){
-
+function IdeLoading(){
+	var N1=1000;
+	var N2=40;
 	$('.nowloading_back').html('<div class="nowloading"><div class="main_circle"><div class="mid_circle"><div class="line_a"></div><div class="line_b"></div><div class="line_c"></div><div class="line_d"></div><div class="line_e"></div><div class="line_f"></div><div class="line_g"></div><div class="point"></div><div class="point2"></div><div class="circle"><svg width="120" height="120"><circle cx="60" cy="60" r="58"></svg></div></div></div><div id="ld0" class="nowloading_text">U</div><div id="ld1" class="nowloading_text">P</div><div id="ld2" class="nowloading_text">L</div><div id="ld3" class="nowloading_text">O</div><div id="ld4" class="nowloading_text">A</div><div id="ld5" class="nowloading_text">D</div><div id="ld6" class="nowloading_text">I</div><div id="ld7" class="nowloading_text">N</div><div id="ld8" class="nowloading_text">G</div><div id="ld9" class="nowloading_text">.</div><div id="ld10" class="nowloading_text">.</div><div id="ld11" class="nowloading_text">.</div></div></div>');
 	setInterval(function(){
 		N1=N1-100;
@@ -30,5 +28,4 @@ $(function(){
 	$('.line_c,.line_b').delay(1600).fadeIn(0).animate({height:'90px'}, 800);
 	$('.line_d,.line_e,.line_f').delay(2000).fadeIn(0).animate({height:'56px'}, 500);
 	$('.line_g').delay(2200).fadeIn(0).animate({width:'70px'}, 600);
-});
-});
+}
