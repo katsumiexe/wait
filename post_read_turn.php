6e144338763e07f9e9180afa5f135c405ee124d6
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-03-24 16:29:24 +0900

    ec

--- post_read_turn.php
+++ post_read_turn.php
@@ -1,19 +1,68 @@
 <?
+
+$unit[0][0]=2;
+$unit[0][1]=3;
+$unit[0][2]=7;
+$unit[0][3]=8;
+
+$unit[1][0]=2;
+$unit[1][1]=4;
+$unit[1][2]=7;
+$unit[1][3]=9;
+
+$unit[2][0]=2;
+$unit[2][1]=5;
+$unit[2][2]=7;
+$unit[2][3]=10;
+
+$unit[3][0]=2;
+$unit[3][1]=6;
+$unit[3][2]=7;
+$unit[3][3]=11;
+
+$unit[4][0]=3;
+$unit[4][1]=4;
+$unit[4][2]=8;
+$unit[4][3]=9;
+
+$unit[5][0]=3;
+$unit[5][1]=5;
+$unit[5][2]=8;
+$unit[5][3]=10;
+
+$unit[6][0]=3;
+$unit[6][1]=6;
+$unit[6][2]=8;
+$unit[6][3]=11;
+
+$unit[7][0]=4;
+$unit[7][1]=5;
+$unit[7][2]=9;
+$unit[7][3]=10;
+
+$unit[8][0]=4;
+$unit[8][1]=6;
+$unit[8][2]=9;
+$unit[8][3]=11;
+
+$unit[9][0]=5;
+$unit[9][1]=6;
+$unit[9][2]=10;
+$unit[9][3]=11;
+
 $turn	=$_REQUEST["turn"];
 $bet	=$_REQUEST["bet"];
 $data_a	=$_REQUEST["data_a"];
 $data_b	=$_REQUEST["data_b"];
 $data_c	=$_REQUEST["data_c"];
 $data_d	=$_REQUEST["data_d"];
-
-$card	=$_REQUEST["card"];
+$data_e	=$_REQUEST["data_e"];
 
 $sort["p"]=$bet;
 $sort["a"]=$data_a;
 $sort["b"]=$data_b;
 $sort["c"]=$data_c;
 $sort["d"]=$data_d;
-
 
 $p=0;
 arsort($sort);
@@ -44,7 +93,24 @@ if($check[0] >$check[1]){
 
 $sort["z"]=$win;
 
+if($unit[$win][0] == $data_e || $unit[$win][1] == $data_e){
+	$sort["pts"]+=3;
 
+}elseif($unit[$win][2] == $data_e || $unit[$win][3] == $data_e){
+	$sort["pts"]-=2;
+
+}elseif($data_e==0){
+	$sort["pts"]+=4;
+
+}elseif($data_e==1){
+	$sort["pts"]+=3;
+
+}elseif($data_e<7){
+	$sort["pts"]+=1;
+
+}else{
+	$sort["pts"]+=2;
+}
 
 echo json_encode($sort);
 ?>
--- test.php
+++ test.php
@@ -83,9 +83,9 @@ session_start();
     background:#906000 !important;
 }
 
-.set{
+.set,.sel{
     display:inline-block;
-    width:40px;
+    width:90px;
 	height:30px;
 	line-height:30px;
 	text-align:right;
@@ -99,73 +99,144 @@ session_start();
 <script>
 var Turn=0;
 var St=[];
-var Rand=[0,1,2,3,4,5,6,7,8,9];
+var Doll=[];
+var Rand=[0,1,2,3,4,5,6,7,8,9,];
+var St_Rand1=[0,1,2,3,4,5,6,7,8,9,10,11];
+var St_Rand2=[0,1,2,3,4,5,6,7,8,9,10,11];
+var St_Rand3=[0,1,2,3,4,5,6,7,8,9,10,11];
+var St_Rand4=[0,1,2,3,4,5,6,7,8,9,10,11];
+var St_Rand5=[0,1,2,3,4,5,6,7,8,9,10,11];
 
+var Pts={
+		"a": 0,
+		"b": 0,
+		"c": 0,
+		"d": 0,
+		"e": 0,
+		"p": 0
+};
+
+var R=0;
 $(function(){ 
     $('.sel').on('click',function(){
-		Doll["p"]=$(this).attr('id').replace('d','');
+		Doll["p"]=$(this).attr('id').replace('s','');
+		for (i =0; i <50; i++) {
+			Rnd_a = Math.floor( Math.random() * 10);
+			Rnd_b = Math.floor( Math.random() * 10);
+			Tmp=Rand[Rnd_a];
+			Rand[Rnd_a]=Rand[Rnd_b];
+			Rand[Rnd_b]=Tmp;
+		}
 
+		if(Rand[R] == Doll["p"]){
+			R++;
+		}
+		Doll["a"]=Rand[R];
+		R++;
+
+		if(Rand[R] == Doll["p"]){
+			R++;
+		}
+		Doll["b"]=Rand[R];
+		R++;
+
+		if(Rand[R] == Doll["p"]){
+			R++;
+		}
+		Doll["c"]=Rand[R];
+		R++;
+
+		if(Rand[R] == Doll["p"]){
+			R++;
+		}
+		Doll["d"]=Rand[R];
+		R++;
     });
 
-    $('.start').on('click',function(){
+	$('.start').on('click',function(){
+		$('.p_pts').addClass('p_pts_on');
 
-console.log(Rand);
-for (i =0; i <10; i++) {
-	Rnd_a = Math.floor( Math.random() * 10);
-	Rnd_b = Math.floor( Math.random() * 10);
-	Tmp=Rand[Rnd_a];
-	Rand[Rnd_a]=Rand[Rnd_b];
-	Rand[Rnd_b]=Tmp;
+		for (i =0; i <40; i++) {
+			Rnd_a = Math.floor( Math.random() * 12);
+			Rnd_b = Math.floor( Math.random() * 12);
+			Tmp=St_Rand1[Rnd_a];
+			St_Rand1[Rnd_a]=St_Rand1[Rnd_b];
+			St_Rand1[Rnd_b]=Tmp;
+		}
+		Up_a=St_Rand1;
 
-console.log(Rnd_a);
-console.log(Rnd_b);
-console.log(tmp);
-console.log("〇");
+		for (i =0; i <30; i++) {
+			Rnd_a = Math.floor( Math.random() * 12);
+			Rnd_b = Math.floor( Math.random() * 12);
+			Tmp=St_Rand2[Rnd_a];
+			St_Rand2[Rnd_a]=St_Rand2[Rnd_b];
+			St_Rand2[Rnd_b]=Tmp;
+		}
+		 Up_b=St_Rand2;
 
+		for (i =0; i <20; i++) {
+			Rnd_a = Math.floor( Math.random() * 12);
+			Rnd_b = Math.floor( Math.random() * 12);
+			Tmp=St_Rand3[Rnd_a];
+			St_Rand3[Rnd_a]=St_Rand3[Rnd_b];
+			St_Rand3[Rnd_b]=Tmp;
+		}
+		Up_c=St_Rand3;
 
-}
-console.log(Rand);
+		for (i =0; i <45; i++) {
+			Rnd_a = Math.floor( Math.random() * 12);
+			Rnd_b = Math.floor( Math.random() * 12);
+			Tmp=St_Rand4[Rnd_a];
+			St_Rand4[Rnd_a]=St_Rand4[Rnd_b];
+			St_Rand4[Rnd_b]=Tmp;
+		}
+		Up_d=St_Rand4;
 
+		for (i =0; i <35; i++) {
+			Rnd_a = Math.floor( Math.random() * 12);
+			Rnd_b = Math.floor( Math.random() * 12);
+			Tmp=St_Rand5[Rnd_a];
+			St_Rand5[Rnd_a]=St_Rand5[Rnd_b];
+			St_Rand5[Rnd_b]=Tmp;
+		}
+		Up_e=St_Rand5;
 
-        $('.p_pts').addClass('p_pts_on');
-		$.post({
-			url:'post_start.php',
-			dataType: 'json',
-
-		}).done(function(data1, textStatus, jqXHR){
-		console.log(data1.a);
-			Up=data1;
-            $('#set_a').text(Up.a[0]);
-            $('#set_b').text(Up.b[0]);
-            $('#set_c').text(Up.c[0]);
-            $('#set_d').text(Up.d[0]);
-		});
-    });
+			console.log(Up_a)
+			console.log(Up_b)
+			console.log(Up_c)
+			console.log(Up_d)
+			console.log(Up_e)
+	});
 
     $('.td_b2').on('click','.p_pts_on',function(){
         $(this).removeClass('p_pts_on');
         No=$(this).attr('id').replace('i','');
 
-		console.log(Up);
 		$.post({
 			url:'post_read_turn.php',
 			dataType: 'json',
 			data:{
 				'turn':Turn,
 				'bet':No,
-				'data_a':Up.a[Turn],
-				'data_b':Up.b[Turn],
-				'data_c':Up.c[Turn],
-				'data_d':Up.d[Turn],
+				'data_a':Up_a[Turn],
+				'data_b':Up_b[Turn],
+				'data_c':Up_c[Turn],
+				'data_d':Up_d[Turn],
+				'data_e':Up_e[Turn],
 			},
 		}).done(function(data2, textStatus, jqXHR){
-			console.log(data2);
+			Pts[data2.z]=parseInt(Pts[data2.z])+parseInt(data2.pts);
+
             $('#set_z').text(data2.z);
-            $('#set_a').text(data2.a);
-            $('#set_b').text(data2.b);
-            $('#set_c').text(data2.c);
-            $('#set_d').text(data2.d);
+            $('#set_a').text(data2.a +'□'+Pts['a']);
+            $('#set_b').text(data2.b +'□'+Pts['b']);
+            $('#set_c').text(data2.c +'□'+Pts['c']);
+            $('#set_d').text(data2.d +'□'+Pts['d']);
             $('.td_a').text(Turn);
+
+			console.log(data2.pts);
+			console.log(Pts);
+
 		});
         Turn++;
     });
@@ -173,49 +244,14 @@ console.log(Rand);
 </script>
 </head>
 <body style="text-align:center">
-<!--
-St[0][0]="みりあ";
-St[0][1]=1;
-St[0][2]=2;
 
-St[1][0]="みりあ";
-St[1][1]=1;
-St[1][2]=3;
 
-St[2][0]="みりあ";
-St[2][1]=1;
-St[2][2]=4;
-
-St[3][0]="みりあ";
-St[3][1]=1;
-St[3][2]=5;
-
-St[4][0]="みりあ";
-St[4][1]=2;
-St[4][2]=3;
-
-St[5][0]="みりあ";
-St[5][1]=2;
-St[5][2]=4;
-
-St[6][0]="みりあ";
-St[6][1]=2;
-St[6][2]=5;
-
-St[7][0]="みりあ";
-St[7][1]=3;
-St[7][2]=4;
-
-St[8][0]="みりあ";
-St[8][1]=3;
-St[8][2]=5;
-
-St[9][0]="みりあ";
-St[9][1]=4;
-St[9][2]=5;
--->
 <div class="main">
 <div class="start">START</div>
+<?for($e=0;$e<10;$e++){?>
+<span id="s<?=$e?>" class="sel"><?=$e?></span>
+<?}?>
+<hr>
 <span id="set_a" class="set">0</span>
 <span id="set_b" class="set">0</span>
 <span id="set_c" class="set">0</span>
